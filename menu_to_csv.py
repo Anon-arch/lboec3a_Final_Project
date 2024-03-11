5d4cc8c7fd2ff9eb0f894f436a3b46b830fd7594
Anon-arch <enzocabaero@gmail.com>
2024-03-11 13:56:57 +0800

    price rework fix

--- menu_to_csv.py
+++ menu_to_csv.py
@@ -7,8 +7,5 @@ menuData = pd.DataFrame(menu)
 
 Arranged_menu = menuData.transpose()
 
-convert_cols = ['Tall', 'Grande', 'Venti']
 
-converted_menu = (Arranged_menu[convert_cols] * 55.5).round(2)
-
-print(converted_menu)
+print(Arranged_menu)
