2a1ac6b4749f48bd3f42e7f70954fcfcfa2ad499
Anon-arch <enzocabaero@gmail.com>
2024-03-20 23:21:18 +0800

    simplified code for easier debugging

--- build.h
+++ build.h
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-long menu_prices[38][3] = {
-    //{tall, grande, venti}
+int menu_prices[38][3] = {
+    ///{tall, grande, venti
     // ESPRESSO & MORE
     {130,145,160},    //Caffe Americano,
     {140,155,170},    //Cappuccino,
@@ -74,10 +74,7 @@ char *customizations[] = {"Flavored Syrup", "Espresso Shot", "White Mocha", "Hea
                           "Organic Soymilk", "Breve"};
 
 /*
-returns menu in string to console
-
-
-
+mreturns menu in string to console
 use for loop to iterate items in the accessed in the 3d array using this format
 menu item, tall price, grande price, venti price
 
@@ -91,77 +88,56 @@ void menu_view() {
   printf("ESPRESSO & MORE\n");
   printf("%-80s  |    %-6s    |   %-6s  |   %-6s   |\n", "Espresso Beverages", "Tall", "Grande", "Venti");
 
-  for (int item = 0; item < 8; item++) {
-  printf("|   %-60s                  ", menu_names[item]);
-  printf("|    %6.1li    |   %6.1li  |   %6.1li   |\n",  menu_prices[item][0], menu_prices[item][1], menu_prices[item][2]);
+  for (int item = 0; item < sizeof(menu_prices); item++) {
+
+    // if statements contain category of item
+    if(item == 8 ){
+        printf("\nCold Coffee Craft\n");
+    }
+    if (item == 11)
+    {
+          printf("\nChocolate\n");
+    }
+    if (item == 13)
+    {
+        printf("\nFRAPPUCCINO\n");
+        printf("%-80s  |    %-6s    |   %-6s  |   %-6s   |\n", "Blended Coffee", "Tall", "Grande", "Venti");
+    }
+    if(item == 19){
+        printf("\nBlended Cream\n");
+    }
+    if(item == 25){
+
+        printf("\nCustomize Your Beverage\n");
+        printf("ADD\n");
+        for (int custom = 0; custom < 5; custom++)
+        {
+            printf(" - %s +%i\n", customizations[custom], add_ons[custom][0]);
+        }
+
+        printf("Substitute\n");
+        for (int custom = 5; custom < 7; custom++)
+        {
+            printf(" - %s +%i\n", customizations[custom], add_ons[custom][0]);
+        }
+        // -------------
+        printf("\nTEAVANA TEAS\n");
+        printf("%-80s  |    %-6s    |   %-6s  |   %-6s   |\n", "Signature Tea Beverage", "Tall", "Grande", "Venti");
+    }
+    if(item == 28){
+        printf("\nTeavana Iced Shaken Tea\n");
+    }
+    if(item == 31){
+        printf("\nTeavana Tea Latte\n");
+    }
+    if(item == 34){
+        printf("\nTeavana Full-Leaf Brewed Tea\n");
+    }
+    //main iterator of menu items
+    printf("|   %-60s                  ", menu_names[item]);
+    printf("|    %6i    |   %6i  |   %6i   |\n",  menu_prices[item][0], menu_prices[item][1], menu_prices[item][2]);
   }
 
-  printf("\nCold Coffee Craft\n");
-  for (int item = 8; item < 11; item++) {
-  printf("|   %-60s                  ", menu_names[item]);
-  printf("|    %6.1li    |   %6.1li  |   %6.1li   |\n",  menu_prices[item][0], menu_prices[item][1], menu_prices[item][2]);
-  }
-
-  printf("\nChocolate\n");
-  for (int item = 11; item < 13; item++) {
-  printf("|   %-60s                  ", menu_names[item]);
-  printf("|    %6.1li    |   %6.1li  |   %6.1li   |\n",  menu_prices[item][0], menu_prices[item][1], menu_prices[item][2]);
-  }
-
-// -------------
-
-  printf("\nFRAPPUCCINO\n");
-  printf("%-80s  |    %-6s    |   %-6s  |   %-6s   |\n", "Blended Coffee", "Tall", "Grande", "Venti");
-  for (int item = 13; item < 19; item++) {
-  printf("|   %-60s                  ", menu_names[item]);
-  printf("|    %6.1li    |   %6.1li  |   %6.1li   |\n",  menu_prices[item][0], menu_prices[item][1], menu_prices[item][2]);
-  }
-
-  printf("\nBlended Cream\n");
-  for (int item = 19; item < 25; item++) {
-  printf("|   %-60s                  ", menu_names[item]);
-  printf("|    %6.1li    |   %6.1li  |   %6.1li   |\n",  menu_prices[item][0], menu_prices[item][1], menu_prices[item][2]);
-  }
-
-  printf("\nCustomize Your Beverage\n");
-  printf("ADD\n");
-  for (int custom = 0; custom < 5; custom++)
-  {
-    printf(" - %s +%i\n", customizations[custom], add_ons[custom][0]);
-  }
-
-  printf("Substitute\n");
-  for (int custom = 5; custom < 7; custom++)
-  {
-    printf(" - %s +%i\n", customizations[custom], add_ons[custom][0]);
-  }
-
-// -------------
-
-  printf("\nTEAVANA TEAS\n");
-  printf("%-80s  |    %-6s    |   %-6s  |   %-6s   |\n", "Signature Tea Beverage", "Tall", "Grande", "Venti");
-  for (int item = 25; item < 28; item++) {
-  printf("|   %-60s                  ", menu_names[item]);
-  printf("|    %6.1li    |   %6.1li  |   %6.1li   |\n",  menu_prices[item][0], menu_prices[item][1], menu_prices[item][2]);
-  }
-
-  printf("\nTeavana Iced Shaken Tea\n");
-  for (int item = 28; item < 31; item++) {
-  printf("|   %-60s                  ", menu_names[item]);
-  printf("|    %6.1li    |   %6.1li  |   %6.1li   |\n",  menu_prices[item][0], menu_prices[item][1], menu_prices[item][2]);
-  }
-
-  printf("\nTeavana Tea Latte\n");
-  for (int item = 31; item < 34; item++) {
-  printf("|   %-60s                  ", menu_names[item]);
-  printf("|    %6.1li    |   %6.1li  |   %6.1li   |\n",  menu_prices[item][0], menu_prices[item][1], menu_prices[item][2]);
-  }
-
-  printf("\nTeavana Full-Leaf Brewed Tea\n");
-  for (int item = 34; item < 38; item++) {
-  printf("|   %-60s                  ", menu_names[item]);
-  printf("|    %6.1li    |   %6.1li  |   %6.1li   |\n",  menu_prices[item][0], menu_prices[item][1], menu_prices[item][2]);
-  }
 }
 
 
@@ -169,8 +145,8 @@ void menu_view() {
 
 must return an 2d array that contains index of order and customizations chosen
 
-{{order number}
-{customizations}}
+//{{order number}
+//{customizations}}
 
 for add-ons
 
